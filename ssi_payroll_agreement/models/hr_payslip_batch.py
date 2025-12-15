5d5fc235a88ad32c29beba0ae547a891d2f5f206
Andhitia Rama <andhitia.r@gmail.com>
2025-12-15 07:05:33 +0700

    [UPD] ssi_payroll_agreement
    * Pre-commit fix

--- ssi_payroll_agreement/models/hr_payslip_batch.py
+++ ssi_payroll_agreement/models/hr_payslip_batch.py
@@ -2,8 +2,7 @@
 # Copyright 2022 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
 
-from odoo import _, api, fields, models
-from odoo.exceptions import UserError
+from odoo import models
 
 
 class HrPayslipBatch(models.Model):
@@ -15,7 +14,7 @@ class HrPayslipBatch(models.Model):
     def _trigger_onchange(self, payslip):
         self.ensure_one()
         _super = super(HrPayslipBatch, self)
-        res = _super._trigger_onchange(payslip)
+        _super._trigger_onchange(payslip)
         payslip.onchange_payroll_agreement_id()
         payslip.onchange_aggrement_structure_id()
         payslip.onchange_structure_id()
